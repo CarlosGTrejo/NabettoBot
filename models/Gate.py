6bb5dfb64b640eb7c6424a6b911186ea553533ec
Carlos <carlos.guadarrama.trejo@gmail.com>
2019-06-25 18:09:12 -0500

    Created CREDENTIALS and CHANNEL variables
    User and Pass values are now in CREDENTIALS with proper encoding.
    Also fixed sendMessage because the messages were str not byte.

--- models/Gate.py
+++ models/Gate.py
@@ -3,12 +3,19 @@ from models.Fetch import fetchSettings
 
 settings = fetchSettings()
 
-def openGate():
+def openGate() -> "Socket Obj":
     gate = socket.socket()
     gate.connect((settings.HOST, settings.PORT))
-    gate.send("PASS " + settings.PASS + "\r\n")
-    gate.send("NICK " + settings.USER + "\r\n")
-    gate.send("JOIN " + settings.CHANNEL + "\r\n")
+    CREDENTIALS = ("PASS " + settings.PASS + "\r\n"+\
+                   "NICK " + settings.USER + "\r\n").encode()
+                       
+    CHANNEL     = ("JOIN " + settings.CHANNEL + "\r\n").encode()
+    gate.send(CREDENTIALS)
+    gate.send(CHANNEL)
     
     return gate
 
+def sendMessage(gate, msg):
+    envelope = ("PRIVMSG #" + settings.CHANNEL + " :" + msg + "\r\n").encode()
+    gate.send(envelope)
+    print("Sent: " + msg)
