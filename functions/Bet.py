a5d091a030672a43ac44a7b5b1ba25f6424f8e1a
Carlos <carlos.guadarrama.trejo@gmail.com>
2019-07-26 01:37:24 -0500

    Improved Bet Class
    The Bet class now stores bet data in a static variable (__static_data__) and automatically updates it everytime a bet object is created.
    I also added 2 static methods, reset() and betWithMajority.
    
    betWithMajority is the remake of majorityBet and still returns the same output.
    
    reset resets the __static_data__ variable to {"BLUE":0, "RED":0}
    
    I also added a class method named *extractBet* which is the remake of betExtract with an implementation of logBet (which is a remake of logData) and f-strings

--- functions/Bet.py
+++ functions/Bet.py
@@ -1,58 +1,40 @@
-from functions.Tools import logData
+from functions.Tools import logBet
 from models.Connection import settings
 
 
 class Bet:
     """The class stores all basic information collected from each betting session."""
+
+    __static_data__: "Holds the total bet amount for each team" = {"BLUE":0, "RED":0}
+
     def __init__(self, user, team, bet_amount): # Constructor
         self.user: "Person that placed the bet" = user
         self.team: "Team the person bet on" = team
         self.bet_amount: "Amount person bet" = bet_amount
 
-@logData
-def betExtract(message) -> ("username", "team", "amount"):
-    """This function extracts data from messages and return a tuple that has all the data."""
-    split_point: "Point to split the message" = "PRIVMSG #" + settings.CHANNEL + " :"
-    # Extract username
-    username = message.split(split_point)[1].split("@")[1].split(" - ")[0]
-    # Extract team
-    team = "BLUE" if "BLUE" in message else "RED"
-    # Extract amount
-    amount = int(message.split('-').pop().split('.').pop(0).split(', ')[1])
-    return (username, team, amount)
+        __static_data__[self.team] += self.bet_amount # Adding bet amount to the appropriate team.
 
+    @staticmethod
+    def reset() -> None:
+        """Resets the data variable to {BLUE:0, RED:0} for the next betting session"""
+        Bet.__static_data__.update(BLUE=0, RED=0) # Resetting the data variable.
 
-def majorityBet(bets, current_amount):
-    """This function takes data from a Bet class list and the current balance.
-    It returns a bet statement based on the team with the most money and the
-    current amount of money the owner has."""
+    @classmethod
+    def extractBet(cls,message: "A betting request message") -> "Bet Object":
+        """Extracts bet data from a message and updates the __static_data__ variable based on the chosen team"""
+        split_point: "Where the message is split" = "PRIVMSG #" + settings.CHANNEL + " :"
+        # Extract username
+        username = message.split(split_point)[1].split("@")[1].split(" - ")[0]
+        # Extract team
+        team = "BLUE" if "BLUE" in message else "RED"
+        # Extract amount
+        amount = int(message.split('-').pop().split('.').pop(0).split(', ')[1])
+        bet_info = (username, team, amount)
+        logBet(bet_info)
 
-    blue, red = 0, 0 # assume bet cannot be a negative number
-    final_bet_amount: "Current amount * 0.1" = current_amount * 0.1 # each bet is 10% of the total amount owned
+        return cls(*bet_info)
 
-    # Sort and add bets to two baskets: red and blue
-    for bet in bets:
-        if (bet.team == "BLUE"):
-            blue += bet.bet_amount
-        else:
-            red += bet.bet_amount
-
-    # Make the final betting decision based on the majority bet
-    if (blue > red):
-        return "!blue " + str(int(final_bet_amount)) # convert the bet amount to an int because the betting bot only accepts whole numbers
-    else:
-        return "!red " + str(int(final_bet_amount))
-
-def sideWithMoreMoney(bets):
-
-    blue, red = 0, 0 # assume bet cannot be a negative number
-
-    # Sort and add bets to two baskets: red and blue
-    for bet in bets:
-        if (bet.team == "BLUE"):
-            blue += bet.bet_amount
-        else:
-            red += bet.bet_amount
-
-    # Make the final betting decision based on the majority bet
-    print("BLUE:", blue, "RED:", red)
+    @staticmethod
+    def betWithMajority(current_amount: "The current amount of money you hold") -> "!(team) str(current_amount * 0.1)":
+        """Returns a bet command based on the team that holds the highest bet"""
+        return f"!{max(__static_data__).lower} {current_amount * 0.1}"
