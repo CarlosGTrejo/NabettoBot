f0e3c98763c9e86f5daf3ce74f3f6ca618b12ce0
Carlos <carlos.guadarrama.trejo@gmail.com>
2019-07-27 22:19:38 -0500

    Unit tests Implementation
    Current issue is getting it to run without getting the ModuleNotFoundError when importing nabettobot

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+# This is here so that relative package imports work
---
+++ tests/context.py
@@ -0,0 +1,4 @@
+import sys, os, pathlib
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from nabettobot.models import Bet
