b42acd7609da55c8c88b4833b902d574e0dd2f20
Carlos <carlos.guadarrama.trejo@gmail.com>
2019-07-29 01:57:47 -0500

    Implemented new models.py

--- NabettoBot/__main__.py
+++ NabettoBot/__main__.py
@@ -1,18 +1,10 @@
-from datetime import timedelta
 from sys import platform
-from time import perf_counter, sleep # ,time
+from time import sleep
 from traceback import format_exc
-
 from colorama import deinit, init
-
-# from functions.Bet import Bet, betExtract, majorityBet, sideWithMoreMoney
-from nabettobot.functions.Client import messageFormat
-from nabettobot.models.Channel import joinChannel
-from nabettobot.models.Connection import fetchMessages, openConnection
+from .models import Client, Bet
 
 if platform == "win32": from winsound import Beep
-
-
 sequence = (293,200), (293,200), (586,400), (440,400)
 
 def main():
@@ -20,39 +12,33 @@ def main():
     try:
         # Variables for betting
         bet_start = False
-        # bet_done = False
-        # bet_data = () # a Bet class list stores bet data collected and fetches itself to majorityBet
-        PONG = "PONG :tmi.twitch.tv\r\n".encode()
 
         # Opening a new connection
-        connection = openConnection()
-        joinChannel(connection)
-        # keepAlive(connection)
+        client = Client()
+        client.start()
+        client.farm()
 
         # Turn on the bot, begin fetching messages for analysis
         while True:
-            messages = fetchMessages(connection)
+            client.fetchMessages()
 
             # Read each message
-            for message in messages:
-                if (":tmi.twitch.tv" in message):
-                    print(f"\x1b[30m\x1b[103m\n[=] {message}\n")
-                    if ("PING" in message):
-                        Beep(880, 135)
-                        connection.send(PONG)
-                        Beep(587, 135); print('\x1b[30m\x1b[106m\n\t[@] PONG Sent\t\n')
+            for message in client.messages:
+                client.CheckPINGPONG(message)
                 if ("Bet complete" in message):
                     if (bet_start == False): # Creates timestamp for the beginning of the betting session.
-                        print("BETTING SESSION STARTED".center(60, '='))
+                        print(" BETTING SESSION STARTED ".center(70, '='))
                         bet_start = True
                         # timer_start = time()
 
-                    # bet_info = betExtract(message,start=timer_start)
-                    # bet_data.append(Bet(*bet_info))
-                    # bet_data += Bet(*bet_info),
+                    Bet.extractBet(message)
+                
+                elif ("Betting has ended" in message):
+                    print(" BETTING SESSION ENDED ".center(70,'='))
+                    Bet.resetData()
 
                 else:
-                    print(timedelta(seconds = int(perf_counter())), messageFormat(message))
+                    client.display(message)
 
                 # if (time() - timer_start > 180 and bet_start == True):
                 #     sideWithMoreMoney(bet_data)
@@ -65,15 +51,6 @@ def main():
                 #     bet_done = True
                 #     print("[+] Finished Betting Session.")
 
-                # # Send out PONG message to twitch server to keep the bot alive
-                # if "PING" in message: # Send out a pong message every time there's a PING
-                #     keepAlive(connection)
-
-            # Temporary: Renew connection every hour and only do so when a betting session has not started yet
-            # time_difference: "Deta of beginning_time and time()" = round(abs(connection_time - previous_connection_time), 3)
-            # connection_time = time() - one_point_time
-            # if(bet_done == True): # Reset main after each bet session
-            #     messageClear()
     except KeyboardInterrupt:
         print("\x1b[96m[.] Exitting...")
 
