1cddf72fda85cc970461e706e3aa202cb05ae6c3
minhluu2000 <40921064+minhluu2000@users.noreply.github.com>
2019-08-26 21:19:40 -0500

    Update scrape_test.py

--- NabettoBot/scrape_test.py
+++ NabettoBot/scrape_test.py
@@ -3,38 +3,39 @@ from selenium import webdriver
 from getpass import getpass
 from time import sleep
 
-USERNAME = "vgfan1996"
-PASSWORD = "19647328"
+def main():
+    USERNAME = input()
+    PASSWORD = getpass()
 
-# Generate a new link from my.saltyteemo.com to twitch for authentication
-scraper = cfscrape.CloudflareScraper()
-twitch_url = scraper.get("https://my.saltyteemo.com/").url
+    # Generate a new link from my.saltyteemo.com to twitch for authentication
+    scraper = cfscrape.CloudflareScraper()
+    twitch_url = scraper.get("https://my.saltyteemo.com/").url
 
-# Open chrome in incognito -> access the link
-options = webdriver.ChromeOptions()
-# options.add_argument("--incognito")
+    # Open chrome in incognito -> access the link
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--incognito")
 
-driver = webdriver.Chrome(executable_path=r"C:\Users\Minh Luu\Downloads\chromedriver_win32\chromedriver.exe", options=options)
-driver.get(twitch_url)
+    driver = webdriver.Chrome(executable_path=r"C:\Users\Minh Luu\Downloads\chromedriver_win32\chromedriver.exe", options=options)
+    driver.get(twitch_url)
 
 
-# Authenticate
-username = driver.find_element_by_id("username")
-username.clear()
-username.send_keys(USERNAME)
+    # Authenticate
+    username = driver.find_element_by_id("username")
+    username.clear()
+    username.send_keys(USERNAME)
 
-password = driver.find_element_by_name("password")
-password.clear()
-password.send_keys(PASSWORD)
+    password = driver.find_element_by_name("password")
+    password.clear()
+    password.send_keys(PASSWORD)
 
-driver.find_element_by_class_name("buttons").click()
+    driver.find_element_by_class_name("buttons").click()
 
-sleep(10)
+    sleep(10)
 
-# Get the balance
-balance = driver.find_element_by_id("f1").text
-balance = balance.split("\n")
+    # Get the balance
+    balance = driver.find_element_by_id("f1").text
+    balance = balance.split("\n")
 
-print(balance[1])
+    print(balance[1])
 
-driver.quit()
+    driver.quit()
