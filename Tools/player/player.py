6f8ae5e5502df8ef62014d29f593c7c603678b13
minhluu2000 <40921064+minhluu2000@users.noreply.github.com>
2019-12-31 17:05:26 -0600

    Update player.py

--- Tools/player/player.py
+++ Tools/player/player.py
@@ -1,38 +1,38 @@
 # This file collects player data based on specific needs
 
-from cassiopeia import *
+from cassiopeia import Summoner, Queue, Season, Champion, apply_settings, set_riot_api_key, get_champion_mastery
 from time import sleep
 
+# SETTINGS
 apply_settings(r".\\cass_settings.json")
-set_riot_api_key("RGAPI-970e1256-dfb6-4ea7-bb9d-8c1bf478b1a0") 
+set_riot_api_key("RGAPI-3e25c681-3fee-44fe-81ac-c9a85e456151")
+season = Season.season_9
+queue = Queue.ranked_solo_fives
 
+# CONSTANTS
 RANK_TO_NUMBER = dict(
     Unranked = 0,
-    IronIV = 1,
-    IronIII = 2,
-    IronII = 3,
-    IronI = 4,
-    BronzeIV = 5,
-    BronzeIII = 6,
-    BronzeII = 7,
-    BronzeI = 8,
-    SilverIV = 9,
-    SilverIII = 10,
-    SilverII = 11,
-    SilverI = 12,
-    GoldIV = 13
+    IronIV = 1, IronIII = 2, IronII = 3, IronI = 4,
+    BronzeIV = 5, BronzeIII = 6, BronzeII = 7, BronzeI = 8,
+    SilverIV = 9, SilverIII = 10, SilverII = 11, SilverI = 12,
+    GoldIV = 13, GoldIII = 14, GoldII = 15, GoldI = 16,
+    PlatinumIV = 17, PlatinumIII = 18, PlatinumII = 19, PlatinumI = 20,
+    DiamondIV = 21, DiamondIII = 22, DiamondII = 23, DiamondI = 24,
+    Master = 25,
+    Grandmaster = 26,
+    Challenger = 27
 )
 
 # TEMPORARY DESIGN FOR PLAYER CLASS
 class Player:
-
+    
     def __init__(self, name, region, champion, **kwargs): # kwrags is used so that users don't have to remember the order
         """Stores player data. If one or more fields is not filled out, 
         the constructor automatically sets default value(s) accordingly.
         _name is a mandatory """
-        self._name = name
+        self._summoner = Summoner(name=name, region=region)
+        self._champion = Champion(name=champion, region=region)
         self._region = region
-        self._champion = champion
         self._level = kwargs['level'] if 'level' in kwargs else 0
         self._rank = kwargs['rank'] if 'rank' in kwargs else 0
         self._rank_wr = kwargs['rank_wr'] if 'rank_wr' in kwargs else 0
@@ -43,17 +43,17 @@ class Player:
         """Returns the level of a player. 
         False if that player does not exist."""
         try:
-            self._level = Summoner(name=self._name, region=self._region).level
+            self._level = self._summoner.level
         except:
-            print("Unexpected error. The default value is set to {}".format(self._level))
+            print("Unexpected error. The default value is set to {}.".format(self._level))
         return self._level
     
     def rank(self):
         """Returns the SoloQ rank of a player."""
         try:
-            self._rank = RANK_TO_NUMBER[str(Summoner(name=name, region=region).ranks[Queue.ranked_solo_fives]).replace(" ", "").replace("<", "").replace(">", "")]
+            self._rank = RANK_TO_NUMBER[str(self._summoner.ranks[Queue.ranked_solo_fives]).replace(" ", "").replace("<", "").replace(">", "")]
         except:
-            print("Unexpected error. The default value is set to {}".format(self._rank))
+            print("Unexpected error. The default value is set to {}.".format(self._rank))
         return self._rank
 
     def rank_wr(self):
@@ -61,7 +61,7 @@ class Player:
         -1 if that player does not exist.
         -2 if there is no SoloQ info."""
         try:
-            summoner_soloq_entries = Summoner(name=name, region=region).league_entries[Queue.ranked_solo_fives]
+            summoner_soloq_entries = self._summoner.league_entries[Queue.ranked_solo_fives]
             wins = summoner_soloq_entries.wins
             losses = summoner_soloq_entries.losses
             self._rank_wr = round((wins/(wins + losses) * 100), 1)
@@ -69,19 +69,17 @@ class Player:
             print("This person has not played any ranked SoloQ game yet. The default value is set to {}%".format(self._rank_wr))
         except:
             print("Unexpected error. The default value is set to {}%".format(self._rank_wr))
+        return self._rank_wr
 
-    def champ_wr(self):
+    def champ_wr(self, ngames=5):
         """Returns player's win rate on a specific champ on SoloQ. 
         -1 if that player does not exist.
         -2 if there is no SoloQ info for the specific champion."""
         try:
-            summoner = Summoner(name=name, region=region)
-            season = Season.season_9
-            queue = Queue.ranked_solo_fives
-            matches = summoner.match_history(seasons={season}, queues={queue}, champions={champion})
+            matches = self._summoner.match_history(seasons={season}, queues={queue}, champions={self._champion})
             wins, losses = 0, 0
-            for match in matches:
-                if match.participants[summoner].stats.win:
+            for match in matches[:ngames]:
+                if match.participants[self._summoner].stats.win:
                     wins += 1
                 else:
                     losses += 1
@@ -96,19 +94,17 @@ class Player:
         """Returns player's mastery on a specific champ. 
         -1 if that player does not exist."""
         try:
-            self._champ_mastery = get_champion_mastery(champion=Champion(name=self._champion , region=self._region), summoner=Summoner(name=self._name, region=self._region), region=self._region).points
+            self._champ_mastery = get_champion_mastery(champion=self._champion, summoner=self._summoner, region=self._region).points
         except:
             print("Unexpected error. The default value is set to {}%".format(self._champ_mastery))
         return self._champ_mastery
     
 
-
-
 # FOR TROUBLESHOOTING
-if __name__ == "__main__":
-    # print(solowr("pliuwu", "NA"))
-    print("Analyzing...")
-    # print(champ_wr("pliuwu", "NA", 267))
+# if __name__ == "__main__":
+#     players = [Player("pliuwu", "NA", "Nami"), Player("jinlongr", "NA", "Akali"), Player("ifukcinghateminh", "EUW", "Wukong")]
+#     for player in players:
+#         print(player.champ_mastery())
 
 
 
--- Tools/player/player_test.py
+++ Tools/player/player_test.py
@@ -1,40 +1,39 @@
 # UNIT TESTS FOR PLAYER MODULE
 
 import unittest
-from player import level, solorank, solowr, champ_wr, champ_mastery
+from player import Player
 
 
 
 class TestPlayer(unittest.TestCase):
-    def test_level(self):
-        self.assertEqual(level("pliuwu", "NA"), 267)
-        self.assertEqual(level("jinlongr", "NA"), 249)
-        self.assertEqual(level("minhissohandsome", "LAN"), -1)
-        self.assertEqual(level("pliuwu", "EUW"), -1)
-        self.assertEqual(level("", ""), -1)
-
-    def test_solorank(self):
-        self.assertEqual(solorank("pliuwu", "NA"), "PlatinumIV")
-        self.assertEqual(solorank("jinlongr", "NA"), "DiamondIV")
-        self.assertEqual(solorank("minhissohandsome", "LAN"), "Unranked")
-        self.assertEqual(solorank("pliuwu", "EUW"), "Unranked")
-        self.assertEqual(solorank("", ""), "Unranked")
     
-    def test_solowr(self):
-        self.assertTrue(solowr("pliuwu", "NA"))
-        self.assertTrue(solowr("jinlongr", "NA"))
-        self.assertTrue(solowr("smellcookies", "NA"))
-        self.assertTrue(solowr("nastyafcookies", "NA"))
+    def test_level(self):
+        players = [Player("pliuwu", "NA", "Nami"), Player("jinlongr", "NA", "Akali"), Player("nonamereally", "EUW", "Wukong")]
+        for player in players:
+            self.assertTrue(player.level())
 
-    def test_currentchamp_wr(self):
-        self.assertTrue(champ_wr("pliuwu", "NA", 44))
-        self.assertTrue(champ_wr("jinlongr", "NA", 44))
-        self.assertTrue(champ_wr("smellcookies", "NA", 44))
-        self.assertTrue(champ_wr("nastyafcookies", "NA", 44))
+    # def test_solorank(self):
+    #     self.assertEqual(solorank("pliuwu", "NA"), "PlatinumIV")
+    #     self.assertEqual(solorank("jinlongr", "NA"), "DiamondIV")
+    #     self.assertEqual(solorank("minhissohandsome", "LAN"), "Unranked")
+    #     self.assertEqual(solorank("pliuwu", "EUW"), "Unranked")
+    #     self.assertEqual(solorank("", ""), "Unranked")
+    
+    # def test_solowr(self):
+    #     self.assertTrue(solowr("pliuwu", "NA"))
+    #     self.assertTrue(solowr("jinlongr", "NA"))
+    #     self.assertTrue(solowr("smellcookies", "NA"))
+    #     self.assertTrue(solowr("nastyafcookies", "NA"))
 
-    def test_currentchamp_mastery(self):
-        self.assertTrue(champ_mastery("pliuwu", "NA", "Nami"))
-        self.assertTrue(champ_mastery("jinlongr", "NA", "Akali"))
-        self.assertEqual(champ_mastery("smellcookies", "NA", "Senna"), 0)
-        self.assertEqual(champ_mastery("nastyafcookies", "NA", "Senna"), -1)
+    # def test_currentchamp_wr(self):
+    #     self.assertTrue(champ_wr("pliuwu", "NA", 44))
+    #     self.assertTrue(champ_wr("jinlongr", "NA", 44))
+    #     self.assertTrue(champ_wr("smellcookies", "NA", 44))
+    #     self.assertTrue(champ_wr("nastyafcookies", "NA", 44))
+
+    # def test_currentchamp_mastery(self):
+    #     self.assertTrue(champ_mastery("pliuwu", "NA", "Nami"))
+    #     self.assertTrue(champ_mastery("jinlongr", "NA", "Akali"))
+    #     self.assertEqual(champ_mastery("smellcookies", "NA", "Senna"), 0)
+    #     self.assertEqual(champ_mastery("nastyafcookies", "NA", "Senna"), -1)
         
